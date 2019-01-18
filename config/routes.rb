d7f3cc83eecc6a894e0065a05343f9eb1f8b3c2b
joshsarna <joshsarna@gmail.com>
2019-01-17 18:04:16 -0800

    add user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
+  post "/users" => "users#create"
+
+  post "/sessions" => "sessions#create"
 end
